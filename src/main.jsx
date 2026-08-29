03caf65e90693492a3db6f6484c9573c057077f3
khizarusestate <khizarusestate@gmail.com>
2026-08-29 15:42:11 +0500

    feat: show animated launch experience during app bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AppLaunchScreen from './Components/AppLaunchScreen.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AppLaunchScreen />
     <App />
   </StrictMode>,
 )
